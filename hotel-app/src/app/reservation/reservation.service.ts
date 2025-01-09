2f0093527f8914e3e488fff820987322fa77c76d
juanvaes <juancvaes@gmail.com>
2025-01-08 22:18:43 -0500

    See No reservations available message if no reservations exists

--- hotel-app/src/app/reservation/reservation.service.ts
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -25,18 +25,18 @@ export class ReservationService {
 
     addReservation(reservation: Reservation): void {
       this.reservations.push(reservation);
-      localStorage.setItem("reservation", JSON.stringify(this.reservations));
+      localStorage.setItem("reservations", JSON.stringify(this.reservations));
     }
 
     deleteReservation(id: string): void {
       let index = this.reservations.findIndex(res => res.id === id);
       this.reservations.splice(index, 1);
-      localStorage.setItem("reservation", JSON.stringify(this.reservations));
+      localStorage.setItem("reservations", JSON.stringify(this.reservations));
     }
 
     updateReservation(reservation: Reservation): void {
       let index = this.reservations.findIndex(res => res.id === reservation.id);
       this.reservations[index] = reservation;
-      localStorage.setItem("reservation", JSON.stringify(this.reservations));
+      localStorage.setItem("reservations", JSON.stringify(this.reservations));
     }
 }
