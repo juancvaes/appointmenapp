8bd3df58e8ed46f5c7c6700a766bb26ea6fc2bce
juanvaes <juancvaes@gmail.com>
2025-04-05 22:48:54 -0500

    Create TodoAdd component and render input

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoAddComponent } from './todo-add/todo-add.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TodoAddComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
